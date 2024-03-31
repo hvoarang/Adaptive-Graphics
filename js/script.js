cc6d8d4dfb6793a65ccb863c19124f1e7c303fbf
Denys iElkin <hvoarang@gmail.com>
2024-04-01 01:07:13 +0300

    new fix

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ rangeInput.addEventListener(
   'input',
   e => (output.textContent = e.target.value)
 );
+
+let x = true === [];
+let y = true === ![];
+console.log(x + y);
+
+let x = +'5';
+let y = -'5';
+let z = +'5';
+console.log(x + y * z);
+
+let a = 0;
+let b = [];
+console.log(a == b);
+
+const obj = { prop: 10 };
+const arr = [obj];
+arr[0].prop = 20;
+console.log(obj.prop);
